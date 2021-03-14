90b47c41fe236a3e23db55daaacccd533c73fee7
alexferrante <36389437+alexferrante@users.noreply.github.com>
2021-03-14 06:39:09 -0400

    Add changes for processing custom data

--- main.py
+++ main.py
@@ -2,45 +2,9 @@
 '''
 from PhIREGANs import *
 
-# WIND - LR-MR
-#-------------------------------------------------------------
-
-data_type = 'wind'
-data_path = 'example_data/wind_LR-MR.tfrecord'
-model_path = 'models/wind_lr-mr/trained_gan/gan'
-r = [2, 5]
-mu_sig=[[0.7684, -0.4575], [4.9491, 5.8441]]
-
-
-# WIND - MR-HR
-#-------------------------------------------------------------
-'''
-data_type = 'wind'
-data_path = 'example_data/wind_MR-HR.tfrecord'
-model_path = 'models/wind_mr-hr/trained_gan/gan'
-r = [5]
-mu_sig=[[0.7684, -0.4575], [5.02455, 5.9017]]
-'''
-
-# SOLAR - LR-MR
-#-------------------------------------------------------------
-'''
-data_type = 'solar'
-data_path = 'example_data/solar_LR-MR.tfrecord'
-model_path = 'models/solar_lr-mr/trained_gan/gan'
-r = [5]
-mu_sig=[[344.3262, 113.7444], [370.8409, 111.1224]]
-'''
-
-# SOLAR - MR-HR
-#-------------------------------------------------------------
-'''
-data_type = 'solar'
-data_path = 'example_data/solar_MR-HR.tfrecord'
-model_path = 'models/solar_mr-hr/trained_gan/gan'
-r = [5]
-mu_sig = [[344.3262, 113.7444], [386.9283, 117.9627]]
-'''
+data_type = 'st'
+data_path = 'example_data/'
+r = [2]
 
 if __name__ == '__main__':
 
@@ -48,17 +12,15 @@ if __name__ == '__main__':
     
     model_dir = phiregans.pretrain(r=r,
                                    data_path=data_path,
-                                   model_path=model_path,
                                    batch_size=1)
 
     model_dir = phiregans.train(r=r,
                                 data_path=data_path,
-                                model_path=model_dir,
                                 batch_size=1)
     
-    phiregans.test(r=r,
-                   data_path=data_path,
-                   model_path=model_dir,
-                   batch_size=1)
+    # phiregans.test(r=r,
+    #                data_path=data_path,
+    #                model_path=model_dir,
+    #                batch_size=1)
 
 
